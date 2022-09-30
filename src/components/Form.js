ac30e899a29e719a340daee2e0e604bea69c07d5
Realmunene <josep.mwangi@student.moringaschool.com>
2022-09-30 17:28:22 +0300

    modified

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,13 +1,27 @@
 import React, { useState } from "react";
-
 function Form() {
   const [firstName, setFirstName] = useState("John");
   const [lastName, setLastName] = useState("Henry");
-
+  const [newsletter, setNewsletter] = useState(false);
+function handleFirstNameChange(event){
+  setFirstName(event.target.value);
+}
+function handleLastNameChange(event){
+  setLastName(event.target.value);
+}
+function handleNewsletterChange(event){
+setNewsletter(event.target.value);
+}
   return (
     <form>
-      <input type="text" value={firstName} />
-      <input type="text" value={lastName} />
+      <lable htmlFor="newsletter">Subscribe to our Newsletter?</lable>
+      <input type="checkbox"
+      id="newsletter"
+      onChange={handleNewsletterChange}
+      checked ={newsletter}
+      /><br/>
+      <input type="text" onChange = {handleFirstNameChange} value={firstName} />
+      <input type="text" onChange = {handleLastNameChange} value={lastName} />
       <button type="submit">Submit</button>
     </form>
   );
